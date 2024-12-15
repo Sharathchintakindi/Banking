c21c850e506f206904a621c8a11e8ce7f8542e95
Sharathchintakindi <sharath.chinthas@gmail.com>
2024-12-14 23:00:29 -0600

    second day of work

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,25 @@
+import MobileNav from "@/components/ui/MobileNav";
+import SideBar from "@/components/ui/SideBar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Andrian", lastName: "jsm" };
   return (
-    <main> 
-      SIDEBAR 
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <SideBar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+        </div>
+        <div className="absolute top-16 right-10">
+          <MobileNav user={loggedIn} />
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import HeaderBox from "@/components/ui/HeaderBox";
+import RightSideBar from "@/components/ui/RightSideBar";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Adrian",
+    lastName: "JSM",
+    email: "contact@jsmastery.pro",
+  };
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transcations efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.1}
+          />
+        </header>
+        RECENT TRANSCATIONS
+      </div>
+      <RightSideBar user={loggedIn} transactions={[]} banks={[]} />
+    </section>
+  );
+};
 
 export default Home;
